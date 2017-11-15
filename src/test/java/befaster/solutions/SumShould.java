532957495402c15a9ffef4c16d002c436cb8368a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 22:28:46 +0000

    Wed Nov 15 22:28:46 GMT 2017

--- src/test/java/befaster/solutions/SumShould.java
+++ src/test/java/befaster/solutions/SumShould.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class SumTest {
+public class SumShould {
 
     @Test
     public void compute_sum() {
