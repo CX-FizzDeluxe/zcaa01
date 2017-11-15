257dfe6a3613c87a6f9b84770ed58db2463356a0
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 23:22:08 +0000

    Wed Nov 15 23:22:08 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -2,18 +2,31 @@ package befaster.solutions;
 
 public class FizzBuzz {
 
+    private static final String FIVE = "5";
+    private static final String THREE = "3";
+
     public static String fizzBuzz(Integer number) {
-        if (number % 15 == 0 || (contains("3", number) && contains("5", number))) {
+        if (isFizzBuzz(number)) {
             return "fizz buzz";
-        } else if (number % 3 == 0 || contains("3", number)) {
+        } else if (isFizz(number)) {
             return "fizz";
-        } else if (number % 5 == 0 || contains("5", number)) {
-            "buzz";
-        } else {
-            result.append(number);
+        } else if (isBuzz(number)) {
+            return "buzz";
         }
 
-        return result.toString();
+        return String.valueOf(number);
+    }
+
+    private static boolean isBuzz(Integer number) {
+        return number % 5 == 0 || contains(FIVE, number);
+    }
+
+    private static boolean isFizz(Integer number) {
+        return number % 3 == 0 || contains(THREE, number);
+    }
+
+    private static boolean isFizzBuzz(Integer number) {
+        return number % 15 == 0 || (contains(THREE, number) && contains(FIVE, number));
     }
 
     private static boolean contains(String value, Integer number) {
