e3e4cc7a164a27435f15198543b56fbcf77db3f3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 22:31:46 +0000

    Wed Nov 15 22:31:46 GMT 2017

--- src/test/java/befaster/solutions/SumShould.java
+++ src/test/java/befaster/solutions/SumShould.java
@@ -8,7 +8,12 @@ import static org.hamcrest.Matchers.equalTo;
 public class SumShould {
 
     @Test
-    public void compute_sum() {
+    public void compute_sum_of_two_valid_integers() {
         assertThat(Sum.sum(1, 1), equalTo(2));
+    }
+
+    @Test
+    public void throw_an_exception_when_first_param_is_not_betwee_zero_and_100() {
+        assertThat(, is());
     }
 }
