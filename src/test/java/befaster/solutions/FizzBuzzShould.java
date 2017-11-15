3bc4af70a5461f79a9f34c2657cb6b0d455bf74e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 23:26:58 +0000

    Wed Nov 15 23:26:58 GMT 2017

--- src/test/java/befaster/solutions/FizzBuzzShould.java
+++ src/test/java/befaster/solutions/FizzBuzzShould.java
@@ -38,6 +38,11 @@ public class FizzBuzzShould {
     }
 
     @Test
+    public void return_fizz_buzz_when_given_number_contains_five_and_multiple_of_three() {
+        assertThat(FizzBuzz.fizzBuzz(546), is("fizz buzz"));
+    }
+
+    @Test
     public void return_the_number_when_given_number_is_not_multiple_of_three_or_five() {
         assertThat(FizzBuzz.fizzBuzz(1), is("1"));
     }
